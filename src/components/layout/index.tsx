8264c1d871920eba0943b0335fd84e700019716f
dylansebastianm <dylan.sebastianmte@gmail.com>
2025-02-24 11:37:33 -0300

    clean code

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+import React, { useEffect } from "react";
 import Nav from "../nav";
 import Footer from "../footer";
 import Drawer from "../drawer";
@@ -11,11 +11,23 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+
+    useEffect(() => {
+        if (isDrawerOpen) {
+            document.body.style.overflow = "hidden"; // Bloquea el scroll en toda la página
+            document.body.style.height = "100vh";
+        } else {
+            document.body.style.overflow = "auto"; // Restaura el scroll cuando se cierra
+            document.body.style.height = "auto";
+        }
+    }, [isDrawerOpen]);
 
     return (
-        <div className={styles.layout}>
-            <Nav onMenuClick={() => setIsDrawerOpen(true)} />
+        <div className={`${styles.layout} ${isDrawerOpen ? styles.drawerOpen : ""}`}>
+            <div className={styles.navWrapper}>
+                <Nav onMenuClick={() => setIsDrawerOpen(true)} />
+            </div>
             {isDrawerOpen && <Drawer sections={SECTIONS} onClose={() => setIsDrawerOpen(false)} />}
             <main className={styles.main}>
                 <div className={styles.contentContainer}>{children}</div> 
